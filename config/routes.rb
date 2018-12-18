a415cab9e247712b53e15f2362ac11f73df48e2c
KaemonL <kaemonlovendahl14@hotmail.com>
2018-12-18 14:44:14 -0700

    Changed routes for register, login, and logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'pages#home'
-
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+  
   get 'contact', to: 'pages#contact'
   get 'about', to: 'pages#about'
+
+  root to: 'pages#home'
 end
